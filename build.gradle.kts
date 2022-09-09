1767436bb33f19ca74e2d1dedb04e7930c102f3d
Arturo López <lgzarturo@gmail.com>
2022-09-09 01:01:46 -0500

    Cambiar el puerto y se agrega la dependencia devtools con live reload

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
