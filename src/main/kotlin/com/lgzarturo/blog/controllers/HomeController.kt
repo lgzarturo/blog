1c60368d1484c5dd66ee7e8bc05a0b9c25299291
Arturo López <lgzarturo@gmail.com>
2022-09-09 02:36:21 -0500

    Aceptando parámetros en body y en querystring

--- src/main/kotlin/com/lgzarturo/blog/controllers/HomeController.kt
+++ src/main/kotlin/com/lgzarturo/blog/controllers/HomeController.kt
@@ -3,7 +3,9 @@ package com.lgzarturo.blog.controllers
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping
@@ -13,11 +15,15 @@ class HomeController {
     private val log = LoggerFactory.getLogger(this::class.java)
 
     @GetMapping
-    fun index(): Response {
-        log.debug("Procesando la solicitud GET de la función index()")
+    fun index(@RequestBody request: Request?, @RequestParam name: String?): Response {
+        log.debug("Procesando la solicitud GET de la función index() name={}", name)
+        var message = "Hola mundo ${name?:"!"}"
+        if (request != null) {
+            message += ", tenemos un mensaje de ${request.name} y tiene ${request.age} años de antigüedad."
+        }
         return Response(
             status = HttpStatus.OK.value(),
-            message = "Hola mundo"
+            message = message
         )
     }
 }
@@ -25,4 +31,9 @@ class HomeController {
 class Response(
     var status: Int? = null,
     var message: String? = null
+)
+
+class Request(
+    var name: String? = null,
+    var age: Int? = null
 )
