85231402f3bebd64df2d2aa44081c8225ac72e2e
Arturo López <lgzarturo@gmail.com>
2022-11-17 01:52:31 -0500

    Se define la configuración de Spring Security
    - Permitimos acceso a todos los endpoints de la aplicación.
    - Desactivamos el token CSRF para envio de formularios que se requiere en aplicaciones web.
    - Con estos cambios las pruebas de integración vuelven a funcionar.

---
+++ src/main/kotlin/com/lgzarturo/blog/security/SecurityConfiguration.kt
@@ -0,0 +1,18 @@
+package com.lgzarturo.blog.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfiguration {
+    /**
+     * Se le permite el acceso a cualquier endpoint de la aplicación
+     * Con esto ya esté configurado Spring Security, pero de forma
+     * específica estamos dando acceso libre.
+     */
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain =
+        http.csrf().disable().authorizeRequests().anyRequest().permitAll().and().build()
+}
