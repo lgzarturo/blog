669665762505290daa0e44ae2510b1fb85f94a88
Arturo López <lgzarturo@gmail.com>
2022-11-28 08:35:33 -0500

    Aplicar filtros a un Flux

---
+++ src/test/kotlin/com/lgzarturo/blog/webflux/FluxFilters.kt
@@ -0,0 +1,25 @@
+package com.lgzarturo.blog.webflux
+
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Flux
+import reactor.test.StepVerifier
+
+class FluxFilters {
+    private val names: List<String> = listOf("Bellami", "Bartolomeo", "Vivi", "Pika", "Brook", "Crocodile")
+
+    @Test
+    fun fluxFilterTest() {
+        val namesWithB = Flux.fromIterable(names).filter { x: String? -> x?.startsWith("B") ?: false }.log()
+        StepVerifier.create(namesWithB)
+            .expectNext("Bellami", "Bartolomeo", "Brook")
+            .verifyComplete()
+    }
+
+    @Test
+    fun fluxFilterTest_byLength() {
+        val namesWithB = Flux.fromIterable(names).filter { x: String? -> x?.length!! > 5 }.log()
+        StepVerifier.create(namesWithB)
+            .expectNext("Bellami", "Bartolomeo", "Crocodile")
+            .verifyComplete()
+    }
+}
